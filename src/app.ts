31959b77e5ca1c563c3e8ed11d0f3674f6b120c5
zyb8023 <fengying11734@126.com>
2020-12-17 00:02:07 +0800

    ts 环境配置

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log(1111111111111);
+import * as koa from "koa";
+import koaBody from "koa-body";
+
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(async(ctx: koa.BaseContext) => {
+    ctx.body = 'qqqqqq';
+})
+
+app.listen(3000, () => {
+    console.log('server srart at 3000')
+})
